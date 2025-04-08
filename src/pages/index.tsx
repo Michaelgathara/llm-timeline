4063b78210a83803fadc453bb503dc783826333f
Michaelgathara <mikegtrm@gmail.com>
2025-04-07 21:36:45 -0500

    NextJS template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import TimelinePage from "src/components/timeline";
 
 
 export default function Home() {
@@ -7,6 +6,5 @@ export default function Home() {
     <Head>
       <title>LLM Timeline</title>
     </Head>
-    <TimelinePage />
   </>
 }
