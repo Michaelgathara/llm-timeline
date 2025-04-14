c436f76804f26e8469943f6c48a71d32ae06eb06
michaelgathara <mikegtrm@gmail.com>
2025-04-14 16:01:20 -0500

    forgot meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content='LLM Timeline - Explore the LLM landscape visually'/>
+        <meta name="keywords" content="LLMs, LLM Visualization, LLM Graph, LLM Progress" />
+        <meta name="author" content="Michael Gathara" />
+        <title>LLM Timeline</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
